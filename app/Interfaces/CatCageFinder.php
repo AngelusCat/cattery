0001145b5bb0d9763c5865df4482fff7165ffec0
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2025-01-13 13:55:27 +0300

    ...

--- app/Interfaces/CatCageFinder.php
+++ app/Interfaces/CatCageFinder.php
@@ -5,6 +5,10 @@ namespace App\Interfaces;
 use App\Entities\Cage;
 use App\Entities\Cat;
 
+/**
+ * Интерфейс для алгоритма, который ищет подходящую клетку для кошки
+ */
+
 interface CatCageFinder
 {
     public function find(Cat $cat): Cage;
--- app/Services/Strategy/CatCageFinder/CatCageFinderUsingDbSorting.php
+++ app/Services/Strategy/CatCageFinder/CatCageFinderUsingDbSorting.php
@@ -6,6 +6,11 @@ use App\Entities\Cage;
 use App\Entities\Cat;
 use App\Interfaces\CatCageFinder;
 
+/**
+ * На основе состояния кошки определяются критерии фильтрации и сортировки, выполняется SQL-запрос, по данным которого создается определенная Cage
+ * Класс критериев инкапсулирует в себе SQL-запрос
+ */
+
 class CatCageFinderUsingDbSorting implements CatCageFinder
 {
     public function find(Cat $cat): Cage
